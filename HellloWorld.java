dfaf33e19a567ed0de05bbc61f0c9e486bb5ac52
Tristan Hibbard <tristanhibbard@Tristans-MacBook-Air-2.local>
2024-08-27 14:32:36 -0400

    second commit

---
+++ HellloWorld.java
@@ -0,0 +1,13 @@
+
+public class HellloWorld {
+
+	public HellloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
